ba7adada1c11d762883f20133b294c63b1052fe4
Mark Bastourous <mark.nabil.guc@hotmail.com>
2019-05-30 20:29:28 +0200

    the main opencv code

---
+++ opencv_openmv_bridge.py
@@ -0,0 +1,73 @@
+import numpy as np
+import io
+import struct
+import serial
+from PIL import Image as PILImage
+import cv2
+
+# Camera object to create the snaps/frames/images that
+#  will be deserialized later in the opencv code
+class Camera:
+
+    def __init__(self, device='/dev/ttyACM0'):
+        """Reads images from OpenMV Cam
+        Args:
+            device (str): Serial device
+        Raises:
+            serial.SerialException
+        """
+        self.port = serial.Serial(device, baudrate=115200,
+                                  bytesize=serial.EIGHTBITS,
+                                  parity=serial.PARITY_NONE,
+                                  xonxoff=False, rtscts=False,
+                                  stopbits=serial.STOPBITS_ONE,
+                                  timeout=None, dsrdtr=True)
+
+        # Important: reset buffers for reliabile restarts of OpenMV Cam
+        self.port.reset_input_buffer()
+        self.port.reset_output_buffer()
+
+    def read_image(self):
+        """Read image from OpenMV Cam
+        Returns:
+            image (ndarray): Image
+        Raises:
+            serial.SerialException
+        """
+
+        # Sending 'snap' command causes camera to take snapshot
+        self.port.write('snap')
+        self.port.flush()
+
+        # Read 'size' bytes from serial port
+        size = struct.unpack('<L', self.port.read(4))[0]
+        image_data = self.port.read(size)
+        image = np.array(PILImage.open(io.BytesIO(image_data)))
+
+        return image
+
+
+
+currentFrame = 0
+while(True):
+    cap = Camera(device='/dev/ttyACM0')
+    # Capture frame-by-frame
+    im1 = cap.read_image()
+
+    
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+
+    # Saves image of the current frame in jpg file
+    # name = 'frame' + str(currentFrame) + '.jpg'
+    # cv2.imwrite(name, frame)
+
+    # Display the resulting frame
+    cv2.imshow('im1',im1)
+    # cv2.imshow('im1',gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # To stop duplicate images
+    currentFrame += 1
